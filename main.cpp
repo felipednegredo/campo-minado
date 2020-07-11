4a3f6ad4f1b09a62d1547c11247fb610aa0203cf
Felipe de Negredo <felipe.negredo2@gmail.com>
2020-07-11 18:20:25 +0000

    Entradas com os limites

--- main.cpp
+++ main.cpp
@@ -3,13 +3,19 @@
 using namespace std;
 
 int main(){
-  int i,j,bomb;
+  int i,j,bomb,bomb2;
   cout<<"Insira o numero de linhas"<<endl;
   cin>>i;
   cout<<"Insira o numero de colunas"<<endl;
   cin>>j;
-  if (i>1 && j>1)
+  if (i==2&&j==2){
+  bomb=2;
+  }
+  else if (i>2 && j>2){
+  while(bomb>2){
    cout<<"Insira o numero de bombas"<<endl;
-   cin>>bomb;
-
+   cin>>bomb2;
+   bomb=bomb+bomb2;
+  }
+}
 }
